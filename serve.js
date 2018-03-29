eb0d633ff8f38f8995104bdfd1d5734f7d0738c4
secretspy97 <nathan.connor@gosparx.org>
2018-03-29 06:26:31 -0400

    added serve.js

--- serve.js
+++ serve.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static("" + __dirname + "/build/bundled"));
+app.use(express.static("" + __dirname + "/"));
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Running on port '+port);
