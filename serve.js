6c9c97ad531ca5301d1f9a6b90e9e873649d8453
secretspy97 <nathan.connor@gosparx.org>
2018-04-23 22:40:25 -0400

    Fixed routing issue

--- serve.js
+++ serve.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static("" + __dirname + "/"));
+
+app.get('/', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('*', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Running on port '+port);
